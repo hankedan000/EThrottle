13634ad1122d7d7dd6d2911213af34578b38f220
hankedan000 <dany32412@gmail.com>
2023-04-27 19:54:38 -0400

    Adds MSQ broadcast listening logic

--- src/avr/EThrottle/can.h
+++ src/avr/EThrottle/can.h
@@ -8,9 +8,6 @@
 #define CAN_ID  5
 #define CAN_MSG_BUFFER_SIZE 8
 
-inline MegaCAN::CAN_Msg canBuff[CAN_MSG_BUFFER_SIZE];
-inline MegaCAN::ExtDevice canDev(CAN_CS,CAN_ID,CAN_INT,canBuff,CAN_MSG_BUFFER_SIZE,TABLES,NUM_TABLES);
-
 // call this in main arduino setup()
 void
 canSetup();
@@ -18,3 +15,61 @@ canSetup();
 // call this in main arduino loop()
 void
 canLoop();
+
+// extend the base class so we can customize CAN filters and handle standard messages too
+class EThrottleCAN : public MegaCAN::ExtDevice
+{
+public:
+  EThrottleCAN(
+			uint8_t cs,
+			uint8_t myId,
+			uint8_t intPin,
+			MegaCAN::CAN_Msg *buff,
+			uint8_t buffSize,
+			const MegaCAN::TableDescriptor_t *tables,
+			uint8_t numTables);
+
+protected:
+  /**
+   * override this base method so that we can override default options
+   */
+  virtual void
+  getOptions(
+    struct MegaCAN::Options *opts) override;
+
+  /**
+   * override this base method so that we can set filters for broadcast
+   * frame reception (11bit protocol) as well as extended message destined
+   * for the our ID.
+   */
+  virtual void
+  applyCanFilters(
+    MCP_CAN *can) override;
+
+  /**
+   * Called when a standard 11bit megasquirt broadcast frame is received.
+   * 
+   * @param[in] id
+   * The 11bit CAN identifier
+   * 
+   * @param[in] length
+   * The number of data bytes in the CAN frame
+   * 
+   * @param[in] data
+   * A pointer to the data segment of the CAN frame
+   */
+  virtual void
+  handleStandard(
+      const uint32_t &id,
+      const uint8_t &length,
+      uint8_t *data) override;
+
+private:
+
+};
+
+// TODO read from flash
+inline uint16_t ecuRtBcastBaseId = 1520;
+
+inline MegaCAN::CAN_Msg canBuff[CAN_MSG_BUFFER_SIZE];
+inline EThrottleCAN canDev(CAN_CS,CAN_ID,CAN_INT,canBuff,CAN_MSG_BUFFER_SIZE,TABLES,NUM_TABLES);
---
+++ src/avr/EThrottle/ecu_vars.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdint.h>
+
+// ECU variables received via Megasquirt real-time broadcast data
+namespace ecu
+{
+  inline uint16_t rpm;
+}
